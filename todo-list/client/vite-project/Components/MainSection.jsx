eecf8486a9ef14199f24ed934ee47bec46a4e8bc
mihaeldzhigov <pplesho@gmail.com>
2024-07-19 23:26:07 +0300

    form related funcs

--- todo-list/client/vite-project/Components/MainSection.jsx
+++ todo-list/client/vite-project/Components/MainSection.jsx
@@ -1,11 +1,19 @@
 import TodoTable from "./TodoTable"
 import Spinner from "./Spinner"
 import loading from "./TodoTable"
+import { useState } from "react"
 
 export default function MainSection(){
+    const [showed,showForm] = useState(false)
 
     // const
+    function openForm(){
+        showForm(true)
+      }
+    const closeForm =  ()=>{
+        showForm(false)
 
+    }
     return(
          
     <main className="main">
@@ -15,12 +23,12 @@ export default function MainSection(){
             <h1>Todo List</h1>
 
             <div className="add-btn-container">
-                <button className="btn">+ Add new Todo</button>
+                <button className="btn" onClick={openForm}>+ Add new Todo</button>
             </div>
 
             <div className="table-wrapper">
                  
-                <TodoTable/>
+                <TodoTable show = {showed}   closeForm={closeForm}/>
             </div>
         </section>
     </main>
